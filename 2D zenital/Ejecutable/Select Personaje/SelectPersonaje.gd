fc8588eefb8cad0e172b104d90aca3ab2661ef83
juanantoniogit <juanantonio.tecno@gmail.com>
2018-10-30 11:22:04 +0100

    Conecto los botones de seleccion de personajes

--- 2D zenital/Ejecutable/Select Personaje/SelectPersonaje.gd
+++ 2D zenital/Ejecutable/Select Personaje/SelectPersonaje.gd
@@ -1,18 +1,20 @@
 extends Node
 
 func _load_scene(personaje):
-	Global.personaje = load().instance()
+	Global.personaje = load(personaje).instance()
 	Global.personaje.set_name("personaje")
 #	Global.coche.connect("pressed", self, "_back")
 #	print ("coche seleccionado: " + str(car))
-	get_tree().change_scene("res://Menus/SelectNivel/Selec")
+	get_tree().change_scene("res://Ejecutable/Select Niveles/SelectNiveles.tscn")
 
 
 
 
 func _on_Player_1_pressed():
+	_load_scene("res://Ejecutable/Juego/Personajes/Personaje 1/Personaje1.tscn")
 	pass # replace with function body
 
 
 func _on_Player_2_pressed():
+	_load_scene("res://Ejecutable/Juego/Personajes/Personaje 2/Personaje2.tscn")
 	pass # replace with function body
