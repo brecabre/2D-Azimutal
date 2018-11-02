062ca1072d5362b195051bf0489e0e6774468cf0
juanantoniogit <juanantonio.tecno@gmail.com>
2018-11-02 18:55:04 +0100

    multiplico por delta la velocidad

--- 2D zenital/Ejecutable/Juego/Personajes/Personaje 1/Personaje1.gd
+++ 2D zenital/Ejecutable/Juego/Personajes/Personaje 1/Personaje1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 onready var pos = $"Personaje".position
 onready var punto
-var velocidad = 1
+var velocidad = 200
 
 func _ready():
 
@@ -12,22 +12,23 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
-		pos = Vector2(pos.x, pos.y - velocidad)
+		pos.y -= velocidad * delta
 		punto = Vector2(pos.x, pos.y - 2*velocidad)
 	if Input.is_action_pressed("ui_down"):
-		pos = Vector2(pos.x, pos.y + velocidad)
+		pos.y += velocidad * delta
 		punto = Vector2(pos.x, pos.y + 2*velocidad)
 	if Input.is_action_pressed("ui_right"):
-		pos = Vector2(pos.x + velocidad, pos.y)
+		pos.x += velocidad * delta
 		punto = Vector2(pos.x + 2*velocidad, pos.y)
 	if Input.is_action_pressed("ui_left"):
-		pos = Vector2(pos.x - velocidad, pos.y)
+		pos.x -= velocidad * delta
 		punto = Vector2(pos.x - 2*velocidad, pos.y)
 		
 		
-	set_position(Vector2(pos)) 
+	set_position(Vector2(pos)  )
 	#	orientacion, vale así?
 	look_at(Vector2(punto))
+	print ( delta)
 	pass
 	
 	
