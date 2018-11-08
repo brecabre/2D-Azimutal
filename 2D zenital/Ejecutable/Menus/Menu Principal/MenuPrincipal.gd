6a177878734dd6dda5ad04eba12fe97b67d6739f
juanantoniogit <juanantonio.tecno@gmail.com>
2018-11-08 20:45:53 +0100

    quito ready y orden de comenzar la musica

--- 2D zenital/Ejecutable/Menus/Menu Principal/MenuPrincipal.gd
+++ 2D zenital/Ejecutable/Menus/Menu Principal/MenuPrincipal.gd
@@ -3,9 +3,7 @@ extends Node
 var Creditos = preload("res://Ejecutable/Menus/Creditos/Creditos.tscn").instance()
 var Jugar = preload("res://Ejecutable/Menus/Select Personaje/SelectPersonaje.tscn").instance()
 
-func _ready():
-	$Musica.play()
-	pass
+
 
 func _on_Creditos_pressed():
 	get_node("/root/Global Menus").add_child(Creditos)
