b00457a1983f239a67c690602d7fe65ae21de533
juanantoniogit <juanantonio.tecno@gmail.com>
2018-11-08 18:34:35 +0100

    arreglo los creditos

--- 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
+++ 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
@@ -1,8 +1,8 @@
 extends Node
 
-#var MenuPrin = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
+onready var MenuPrin = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
 
 func _on_MenuPrincipal_pressed():
-	get_node("/root/Global Menus").add_child("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn".instance())
+	get_node("/root/Global Menus").add_child(MenuPrin)
 	$".".queue_free()
 	pass
