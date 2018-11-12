8e15e69a5e82f0626b2d9c7a02139b81c249ad5d
CycloPi <venti3d@gmail.com>
2018-11-12 17:53:29 +0100

    Pongo gráfico número de vida

--- 2D zenital/Ejecutable/Juego/HUD/HUD Personaje/HUD.gd
+++ 2D zenital/Ejecutable/Juego/HUD/HUD Personaje/HUD.gd
@@ -5,12 +5,16 @@ extends Node
 # var b = "textvar"
 var preDatos = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Datos_comunes.tscn").instance()
 var GraficaVida
-var posi1vida = Vector2(55,555)
+var posi1vida = Vector2(815,40)
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	GraficaVida = add_child(preDatos)
+	add_child(preDatos)
+	GraficaVida = get_node("/root/partida/HUD/Datos_comunes/Label_datos_comunes")
+	GraficaVida.set_text("Aquí pondré la vida actualizada")
+	GraficaVida.set_position(posi1vida)
+	#
 #	GraficaVida.set_position(Vector2(posi1vida))
 	set_process(true)
 	pass
@@ -20,4 +24,5 @@ func _process(delta):
 #	GraficaVida.set_position(Vector2(posi1vida))
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
+	GraficaVida.set_text("La vida: "+ str(Global.vida))
 	pass
