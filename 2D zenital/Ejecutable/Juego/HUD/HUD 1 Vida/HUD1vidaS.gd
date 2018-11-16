629e7ea6965a97cda19c83ea7915d05dd5bb2a1f
CycloPi <venti3d@gmail.com>
2018-11-16 19:48:17 +0100

    Muevo linea pa que solo lo haga 1 vez

--- 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida/HUD1vidaS.gd
+++ 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida/HUD1vidaS.gd
@@ -10,11 +10,12 @@ func _ready():
 	# Initialization here
 	graf_vida = get_node("/root/partida/HUD/HUD1vidaNodo/Datos_comunes/Label_datos_comunes")
 	graf_vida.set_text("Aquí pondré la vida actualizada")
+	graf_vida.set_position(Vector2(815,60))
 	pass
 
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 	graf_vida.set_text("La vida: "+ str(Global.vida) + " con nodos")
-	graf_vida.set_position(Vector2(815,60))
+	
 #	pass
