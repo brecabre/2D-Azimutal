274d7f7770356a38f24ddad5bba1d7cf11daf341
CycloPi <venti3d@gmail.com>
2018-11-17 12:52:40 +0100

    Maqueando el potenciador

--- 2D zenital/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador1/Potenciador1.gd
+++ 2D zenital/Ejecutable/Juego/Escenarios/Potenciadores/Potenciador1/Potenciador1.gd
@@ -1,23 +1,50 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var prePuntosP1 = preload("res://Ejecutable/Juego/HUD/HUD Datos Comunes/Datos_comunes.tscn").instance()
+var PuntosP1
+var SumaVida = 100
 
-#func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-#	pass
+var tiempoEntra = 20
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+var banderaVisible = false
+var VeloNum =100
+var contColor
+
+
+func _process(delta):
+
+	if banderaVisible:
+		#esto pa mover el numeriko
+		PuntosP1.set_position(Vector2(PuntosP1.get_position().x , PuntosP1.get_position().y+VeloNum*delta))
+		contColor = contColor + delta
+		# esto pal máximo de color llevar cuidao
+		if contColor >= 1:
+			contColor = 1
+			
+		PuntosP1.set("custom_colors/font_color",Color(0.5,0,contColor))
+	
+	else:
+		contColor = delta
+		
+	if OS.get_ticks_msec() - tiempoEntra  > 2000 and banderaVisible:
+		$".".queue_free()
+		print("mato potenciador 1")
+	pass
 
 
 func _on_Area2D_body_entered(body):
-	Global.vida += 20
-	Global.puntos += 100
-	print("entra")
-	$".".queue_free()
-	pass # replace with function body
+#	porsi quieres vida
+#	Global.vida += 20
+	Global.puntos += SumaVida
+	print("entra en el potenciador 1")
+	add_child(prePuntosP1)
+	tiempoEntra = OS.get_ticks_msec()
+	PuntosP1 =get_node("/root/partida/Potenciador1/Datos_comunes/Label_datos_comunes")	
+	PuntosP1.set_position(Vector2($".".position.x , $".".position.y))#	GraficaVida.set_text("Aquí pondré la vida actualizada")
+	PuntosP1.set_text(""+ str(SumaVida) + " puntos")
+	PuntosP1.set("custom_colors/font_color",Color(1,0,0))
+	banderaVisible = true
+	$".".hide()
+
+
+	pass 
