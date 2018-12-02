2c871309809c308e02ce815e653fb8ede13fd9a6
juanantoniogit <juanantonio.tecno@gmail.com>
2018-12-02 21:44:46 +0100

    Añado comentario de que es la bala del enemigo

--- 2D zenital/Ejecutable/Juego/Enemigo/ComunesEmenigo/bala/bala.gd
+++ 2D zenital/Ejecutable/Juego/Enemigo/ComunesEmenigo/bala/bala.gd
@@ -1,21 +1,17 @@
 extends KinematicBody2D
+#bala del player
 
 var speed = 50
 var velocity = Vector2()
 #Establecer el valor para rebote entre 0.5 y 1.1 y añadir limites.
 var rebote = 1
 
-
-
 func apretarGatillo(position, rotation):
 	velocity = Vector2(speed, 0).rotated(rotation)
-
-	
+	print ("pos bala" + str(position))
 	
 
 func _process(delta):
-	
-	
 	
 	
 	var collision = move_and_collide(velocity * delta)
@@ -23,8 +19,9 @@ func _process(delta):
 	if collision:
 		if collision.get_collider().is_in_group("Player"):
 			Global.vida -= 1
+#			print ("bala quita vida")
 		
-		print(collision.get_collider().is_in_group("Player"))
+#		print(collision.get_collider().is_in_group("Player"))
 		queue_free()
 #
 ##________Este codigo es para que la bala pueda rebotar__________
@@ -37,7 +34,7 @@ func _process(delta):
 	pass
 
 #
-#func _on_VisibilityNotifier2D_viewport_exited(viewport):
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
 #	print ("La bala sale de la viewport y desaparece")
-#	queue_free()
-#	pass # replace with function body
+	queue_free()
+	pass 
