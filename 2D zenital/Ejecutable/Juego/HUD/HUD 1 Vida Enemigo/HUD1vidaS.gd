f5e22a69c2c7a9d033e38fe29d6f299f8dbd9c84
juanantoniogit <juanantonio.tecno@gmail.com>
2018-12-18 13:35:58 +0100

    creo la escena del hud del enemigo

---
+++ 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida Enemigo/HUD1vidaS.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var graf_vida
+var Enemigo
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	Enemigo = get_parent().get_parent().get_node("Enemigo")
+	graf_vida = get_node("/root/partida/HUD/HUD1vidaNodo/Datos_comunes/Label_datos_comunes")
+	graf_vida.set_text("Aquí pondré la vida actualizada")
+	graf_vida.set_position(Vector2(100,100))
+	print (Enemigo.vidaMalo)
+	print (Enemigo.position)
+	pass
+
+func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+	graf_vida.set_text("La vida: "+ str(Enemigo.vidaMalo) + " con nodos")
+	print (Enemigo.vidaMalo)
+	
+#	pass
+
