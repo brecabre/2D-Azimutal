cbbbe6d3638787b4b1606e3836d31f4a93b33f35
juanantoniogit <juanantonio.tecno@gmail.com>
2018-12-18 14:10:12 +0100

    HUD vida enemigo

--- 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida Enemigo/HUD1vidaS.gd
+++ 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida Enemigo/HUD1vidaS.gd
@@ -9,12 +9,12 @@ var Enemigo
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	Enemigo = get_parent().get_parent().get_node("Enemigo")
+	Enemigo = get_parent().get_node("Enemigo")
 	graf_vida = get_node("/root/partida/HUD/HUD1vidaNodo/Datos_comunes/Label_datos_comunes")
 	graf_vida.set_text("Aquí pondré la vida actualizada")
 	graf_vida.set_position(Vector2(100,100))
-	print (Enemigo.vidaMalo)
-	print (Enemigo.position)
+#	print (Enemigo.vidaMalo)
+#	print (Enemigo.position)
 	pass
 
 func _process(delta):
