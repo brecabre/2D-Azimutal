1923915414ab61fe81b7708a9e259971509f9cba
CycloPi <venti3d@gmail.com>
2018-12-19 22:47:02 +0100

    Pongo, dirección

--- 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida Enemigo/HUD1vidaS.gd
+++ 2D zenital/Ejecutable/Juego/HUD/HUD 1 Vida Enemigo/HUD1vidaS.gd
@@ -10,9 +10,9 @@ func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
 	Enemigo = get_parent().get_node("Enemigo")
-	graf_vida = get_node("/root/partida/HUD/HUD1vidaNodo/Datos_comunes/Label_datos_comunes")
+	graf_vida = get_node("/root/partida/Enemigo2/HUD1vidaNodo/Datos_comunes/Label_datos_comunes")
 	graf_vida.set_text("Aquí pondré la vida actualizada")
-	graf_vida.set_position(Vector2(Enemigo.global_position.x-50 , global_position.y-60))
+	graf_vida.set_position(Vector2(Enemigo.global_position.x-50 , Enemigo.global_position.y-60))
 #	print (Enemigo.vidaMalo)
 #	print (Enemigo.position)
 	pass
@@ -21,7 +21,7 @@ func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 	graf_vida.set_text("La vida: "+ str(Enemigo.vidaMalo) + " con nodos")
-	print (Enemigo.vidaMalo)
+#	if Enemigo.vidaMalo < 3
 	
 #	pass
 
