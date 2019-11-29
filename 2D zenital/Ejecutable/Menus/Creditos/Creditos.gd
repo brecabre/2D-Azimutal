434fecc9eb690eee8a1ad5a16ca889f037d0115e
CycloPi <venti3d@gmail.com>
2019-11-29 20:56:17 +0100

    error

--- 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
+++ 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
@@ -1,8 +1,10 @@
 extends Node
 
-onready var MenuPrin = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn").instance()
+var preMenuPrin = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
+var MenuPrin
 
 func _on_MenuPrincipal_pressed():
+	MenuPrin = preMenuPrin.instance()
 	get_node("/root/Global Menus").add_child(MenuPrin)
 	get_parent().get_node("boton").play()
 	$".".queue_free()
