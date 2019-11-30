2362fc7c88dd6be3deedb7017ea0faeb6f00800e
CycloPi <venti3d@gmail.com>
2019-11-30 20:04:26 +0100

    Créditos

--- 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
+++ 2D zenital/Ejecutable/Menus/Creditos/Creditos.gd
@@ -1,11 +1,16 @@
 extends Node
 
-var preMenuPrin = load("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
-var MenuPrin
 
-func _on_MenuPrincipal_pressed():
-	MenuPrin = preMenuPrin.instance()
-	get_node("/root/Global Menus").add_child(MenuPrin)
-	get_parent().get_node("boton").play()
-	$".".queue_free()
-	pass
+
+func _ready():
+	escrituraLabel()
+
+func escrituraLabel(): 
+	var sizeX = get_viewport().size.x
+	var sizeY = get_viewport().size.y
+	var file = File.new()
+	file.open("res://Ejecutable/Menus/Creditos/Licencias/Licencias.gd", File.READ)
+	$ScrollContainer/Creditos.set_text(str(file.get_as_text()))
+	$ScrollContainer.set_size(Vector2(sizeX * 0.9, sizeY * 0.8))
+	$ScrollContainer.set_position(Vector2(sizeX * 0.05 , sizeY * 0.15))
+	
